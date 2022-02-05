9137fc33244fad760f013dc40630fa61e1ce8a5c
SergioACV <84293307+SergioACV@users.noreply.github.com>
2022-02-05 00:24:46 -0500

    Diagnosticos
    Mis clases correspondientes
    -Inicio de Sesión
    -Diagnostico 2 y 3 funcionales

---
+++ AplicacionOptimizate/src/optimizate/App.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package optimizate;
+
+/**
+ *
+ * @author sergio andres
+ */
+public class App {
+    
+    public static void main(String[] args){
+        
+        Optimizate app = new Optimizate();
+        
+        app.setVisible(true);
+        
+        
+        
+    }
+    
+}
